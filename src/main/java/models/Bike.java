a2d3975a8498f0b8870cfe803a0772cbe802a958
Yan <jackyan63@gmail.com>
2018-06-01 11:15:49 -0400

    Added bike object

---
+++ src/main/java/models/Bike.java
@@ -0,0 +1,15 @@
+package models;
+
+import java.math.BigDecimal;
+import java.sql.Date;
+
+public class Bike {
+	private String name;
+	private String email;
+	private String phone;
+	private String model;
+	private String serialNumber;
+	private BigDecimal purchasePrice;
+	private Date purchaseDate;
+	private boolean contact;
+}
